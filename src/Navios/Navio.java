b96f333564e70211033949e33b02e05e54c55b2f
jrafa <jrafa@192.168.1.233>
2021-03-14 15:58:07 +0000

    Class Navio

--- src/Navios/Navio.java
+++ src/Navios/Navio.java
@@ -12,4 +12,32 @@ package Navios;
  */
 public class Navio {
     
+    private String matricula;
+    private String nome;
+    private float comprimento;
+
+    public Navio(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public float getComprimento() {
+        return comprimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setComprimento(float comprimento) {
+    this.comprimento = comprimento;
+    }
 }
+
+
